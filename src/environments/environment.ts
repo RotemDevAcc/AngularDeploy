bd610ef93cfc326374421f92244b261846cd38b4
RotemDevAcc <guyron2000@gmail.com>
2023-12-20 12:15:51 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB5Pn5YofFSuuAGgFImNeEYlyFyc0Y9N4o",
+    authDomain: "testproject-58cd8.firebaseapp.com",
+    databaseURL: "https://testproject-58cd8-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "testproject-58cd8",
+    storageBucket: "testproject-58cd8.appspot.com",
+    messagingSenderId: "998512107458",
+    appId: "1:998512107458:web:d4ecfcc95b162490fac05b",
+    measurementId: "G-SKS2R0BSW9"
+  }
+};
